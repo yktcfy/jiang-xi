c645ccdb4ac06112ce20bd26790dda874209842c
yktcfy <zjyktcfy@qq.com>
2025-08-11 15:01:50 +0800

    Update CaijiJiangxiip.sh

--- CaijiJiangxiip.sh
+++ CaijiJiangxiip.sh
@@ -18,7 +18,7 @@ fi
 case $city_choice in
     1)
         city="Jiangxi_dianxin"
-        stream="rtp/239.252.220.242:5140"
+        stream="rtp/239.252.220.238:5140"
         ;;
     0)
         # 逐个处理{ }内每个选项
